792a3ecc326fd8c36dca33011963ea28903ec01d
Martin Grubinger <martin.grubinger@gmail.com>
2019-10-16 13:03:18 +0200

    add screenshot function

---
+++ functions/function-image/function-image.js
@@ -0,0 +1,42 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+// exports.handler = async (event, context) => {
+//   try {
+//     const subject = event.queryStringParameters.name || 'World'
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ message: `Hello ${subject}` })
+//       // // more keys you can return:
+//       // headers: { "headerName": "headerValue", ... },
+//       // isBase64Encoded: true,
+//     }
+//   } catch (err) {
+//     return { statusCode: 500, body: err.toString() }
+//   }
+// }
+
+const puppeteer = require('puppeteer');
+
+exports.handler = async (event, context) => {
+  try {
+    let browser = await puppeteer.launch({ headless: true });
+    let page = await browser.newPage();
+    
+    await page.setViewport({ width: 1024, height: 620 });
+    await page.goto("https://wirdstpkulturhauptstadt2024.grooovinger.com");
+    let s = await page.screenshot({ path: 'buddy-screenshot.png', type: 'png' });
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-type': 'image/jpeg'
+      },
+      body: s.toString('base64'),
+      isBase64Encoded: true
+    }
+    
+    browser.close();
+    console.log('Done!');
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() }
+  }
+}
